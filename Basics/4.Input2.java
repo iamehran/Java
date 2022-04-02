61e68dde832075935c249db07f483a6dd6c085dc
Mehran Firdous <72188268+iamehran@users.noreply.github.com>
2022-04-02 23:46:21 +0530

    Add files via upload

---
+++ Basics/4.Input2.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+public calss Inputs{
+	public static void main(String[] args){
+		Scanner input = new Scanner(System.in)
+		system.out.println("Enter Your rollno.");
+		int rollno = input.nextInt();
+		System.out.println("Your rollno. is"+rollno);
+
+	}
+}
