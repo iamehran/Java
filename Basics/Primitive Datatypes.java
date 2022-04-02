6f7d0a5faec7105f19774b431167692b198fdf57
Mehran Firdous <72188268+iamehran@users.noreply.github.com>
2022-04-02 23:44:33 +0530

    Create Primitive Datatypes.java

---
+++ Basics/Primitive Datatypes.java
@@ -0,0 +1,12 @@
+public class Primitives{
+	public static void main(String[] args){
+		int rollno =123;
+		char letter='m';
+		float marks=23.67f;
+		double largeDnos=121234332.22;
+		long largeintegervalues=55565656895656L;
+		boolean check=true;
+		//String is not a primitive
+
+	}
+}
