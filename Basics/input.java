c0b37635cb9f66bc5f3bca5f292e7229b9ca7eae
Mehran Firdous <72188268+iamehran@users.noreply.github.com>
2022-04-02 23:43:36 +0530

    Create input.java

---
+++ Basics/input.java
@@ -0,0 +1,9 @@
+import java.util.scanner
+public class Main{
+	public static void main(String[] args){
+		System.out.println("hello");
+		Scanner input = new Scanner(System.in);
+		   System.out.println(input.nextint());
+
+	}
+}
